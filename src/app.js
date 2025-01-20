f1c0a2d886e37e595c37cdc1848f4f67f39a9cbc
Rituparno Ghosh <rituparno.g@rdsoftllp.com>
2025-01-20 12:52:19 +0530

    Improve performance with lazy and dynamic loading

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
-import React from "react";
+import React, {lazy,Suspense} from "react";
 import ReactDom from "react-dom/client";
 import Header  from "./components/Header";
 import Body from "./components/Body";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
-import About from "./components/About";
+// import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
 
 
+const About = lazy(() => import("./components/About"));
 
 const AppLayout = () => {
      return (
@@ -35,7 +36,10 @@ const appRouter = createBrowserRouter([
          },
          {
             path: "/about",
-            element: <About />
+            element: (<Suspense fallback={<h1>Loading....</h1>}>
+               <About />
+               </Suspense>
+            )
          },
          {
             path: "/contact",
